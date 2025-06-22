bcd0c80783764c21913194f893a9392604693504
HyunTaek Oh <oht222@naver.com>
2025-06-22 17:00:17 +0900

    add: Card 기본 예제 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Card from './components/Card.tsx';
+import { ActiveCardProvider } from './stores/activeCard/ActiveCardProvider.tsx';
+import { OrientationProvider } from './stores/Orientation.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      React Card CSS Example
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <OrientationProvider>
+          <ActiveCardProvider>
+            <Card
+              id={'swsh12pt5-160'}
+              name={'Pikachu'}
+              number={'160'}
+              set={'swsh12pt5'}
+              types={'Lightning'}
+              img={'https://images.pokemontcg.io/swsh12pt5/160_hires.png'}
+            />
+          </ActiveCardProvider>
+        </OrientationProvider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
